5069aa917ca6655ae8f66604edd53c54263a7fc8
necati.ozdogan <necati.ozdogan@softtech.com.tr>
2022-10-29 15:01:10 +0300

    createe package for main class

--- src/main/java/com/example/dependencyinjectionspringboot/main/DependencyInjectionSpringBootApplication.java
+++ src/main/java/com/example/dependencyinjectionspringboot/main/DependencyInjectionSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.example.dependencyinjectionspringboot;
+package com.example.dependencyinjectionspringboot.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
