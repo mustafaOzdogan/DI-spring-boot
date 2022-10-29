c88834fa467fb0b5e9660e074b7f38b6f474bfaa
necati.ozdogan <necati.ozdogan@softtech.com.tr>
2022-10-29 14:51:28 +0300

    first commit

---
+++ src/test/java/com/example/dependencyinjectionspringboot/DependencyInjectionSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependencyinjectionspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
