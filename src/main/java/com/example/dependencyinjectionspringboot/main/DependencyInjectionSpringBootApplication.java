336fb1dabebc61acad2c74b57085ef48593e52fa
necati.ozdogan <necati.ozdogan@softtech.com.tr>
2022-11-07 09:38:53 +0300

    add component-scan

--- src/main/java/com/example/dependencyinjectionspringboot/main/DependencyInjectionSpringBootApplication.java
+++ src/main/java/com/example/dependencyinjectionspringboot/main/DependencyInjectionSpringBootApplication.java
@@ -2,8 +2,12 @@ package com.example.dependencyinjectionspringboot.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan("com.example.dependencyinjectionspringboot.service")
 public class DependencyInjectionSpringBootApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/dependencyinjectionspringboot/main/DependencyInjectionSpringBootApplicationTests.java
+++ src/test/java/com/example/dependencyinjectionspringboot/main/DependencyInjectionSpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.dependencyinjectionspringboot;
+package com.example.dependencyinjectionspringboot.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
